81fe4ae8383357552df35beb0015b477aee6752a
amiblog <reza.am.0425@gmail.com>
2022-01-14 16:27:31 +0000

    投稿編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get 'books/new' new_book
   #get 'books/index' books
   #get 'books/show' book
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
