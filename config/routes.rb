e1b4366b816bfb59c3d99ad89b9bf0a676c633b0
amiblog <reza.am.0425@gmail.com>
2022-01-14 14:32:20 +0000

    投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  #get 'books/new' new_book
+  #get 'books/index' books
+  #get 'books/show' book
+  resources :books, only: [:new, :create, :index, :show]
   devise_for :users
   root to: 'homes#top'
+  get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
