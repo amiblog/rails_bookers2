7364b99f91c4f83e6fd8954fdbda67fa4e7a3c3f
amiblog <reza.am.0425@gmail.com>
2022-01-14 10:13:05 +0000

    新規登録,ログイン機能追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << "3ac167d3f4f6455fb949dcb716bdcde1.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.active_job.queue_adapter = :inline
+
 end
