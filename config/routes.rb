4fc7ee9ceb971071115333dd97a02503500b6fbf
amiblog <reza.am.0425@gmail.com>
2022-01-17 13:16:59 +0000

    [finish4-25]

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get "homes/about" => "homes#about", as: "about"
+  get "home/about" => "homes#about", as: "about"
   #get 'books/new' new_book
   #get 'books/index' books
   #get 'books/show' book
   resources :books
-  
+
   #get 'users/show' user
   #get 'users/edit' edit_user
   resources :users
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
